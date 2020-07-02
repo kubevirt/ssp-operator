7a4696038ce0684efd8e1232be030ebc5d083f03
Andrej Krejcir <akrejcir@redhat.com>
2020-07-02 14:53:04 +0200

    Generate new API and Controller
    Used these commands to generate new api and controller:
    ./operator-sdk add api        --api-version=ssp.kubevirt.io/v1 --kind=SSP
    ./operator-sdk add controller --api-version=ssp.kubevirt.io/v1 --kind=SSP
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

---
+++ pkg/apis/ssp/group.go
@@ -0,0 +1,6 @@
+// Package ssp contains ssp API versions.
+//
+// This file ensures Go source parsers acknowledge the ssp package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package ssp
---
+++ pkg/apis/ssp/v1/ssp_types.go
@@ -0,0 +1,48 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SSPSpec defines the desired state of SSP
+type SSPSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// SSPStatus defines the observed state of SSP
+type SSPStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SSP is the Schema for the ssps API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=ssps,scope=Namespaced
+type SSP struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SSPSpec   `json:"spec,omitempty"`
+	Status SSPStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SSPList contains a list of SSP
+type SSPList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SSP `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SSP{}, &SSPList{})
+}
