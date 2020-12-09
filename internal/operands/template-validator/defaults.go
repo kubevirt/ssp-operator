e2ddd9d2b4b3f3b808a11180fc5bb70dbec30bac
Omer Yahud <oyahud@redhat.com>
2020-12-09 14:34:07 +0200

    Bumped template validator version to the latest release
    Signed-off-by: Omer Yahud <oyahud@redhat.com>

--- internal/operands/template-validator/defaults.go
+++ internal/operands/template-validator/defaults.go
@@ -1,5 +1,5 @@
 package template_validator
 
 const (
-	defaultTemplateValidatorImage = "quay.io/kubevirt/kubevirt-template-validator:v0.7.0"
+	defaultTemplateValidatorImage = "quay.io/kubevirt/kubevirt-template-validator:v0.9.0"
 )
