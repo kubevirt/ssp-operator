56a126b040a4397f22fa22a12e84ef72ae74f650
Andrej Krejcir <akrejcir@redhat.com>
2021-07-29 16:28:11 +0200

    Move test scripts from OCP CI jobs to automation directory
    The test scripts from OCP CI jobs are moved to this repository.
    Then CI jobs for different branches can be merged into a single job.
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

---
+++ automation/e2e-upgrade-from-old-operator-functests/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# This file runs the tests.
+# It is run from the root of the repository.
+
+# These evn variables are defined by the CI:
+# CI_OPERATOR_IMG - path of the operator image in the local repository accessible on the CI
+# CI_VALIDATOR_IMG - path of the validator image in the local repository accessible on the CI
+
+export IMG=${CI_OPERATOR_IMG}
+export SKIP_CLEANUP_AFTER_TESTS=true
+
+make deploy functest
