7f710e3dd94973a39ef7671f224c0a27d95594a2
Felix Matouschek <fmatouschek@redhat.com>
2022-09-30 13:41:16 +0200

    Quarantine failing DataSource tests
    The following tests fail because of a bug in CDI:
    
    - [test_id:8112] should restore DataSource if DataImportCron removed from SSP CR
    - [test_id:8298] should restore DataSource, when CDI label is removed
    
    See [1] for more information. Until the bug is fixed in the next CDI
    release the mentioned tests need to be quarantined.
    
    [1] https://github.com/kubevirt/containerized-data-importer/issues/2437
    
    Signed-off-by: Felix Matouschek <fmatouschek@redhat.com>

--- tests/dataSources_test.go
+++ tests/dataSources_test.go
@@ -859,7 +859,7 @@ var _ = Describe("DataSources", func() {
 					logObject(dataImportCron.GetKey(), &cdiv1beta1.DataImportCron{})
 				})
 
-				It("[test_id:8112] should restore DataSource if DataImportCron removed from SSP CR", func() {
+				PIt("[QUARANTINE][test_id:8112] should restore DataSource if DataImportCron removed from SSP CR", func() {
 					// Wait until DataImportCron imports PVC and changes data source
 					Eventually(func() bool {
 						cron := &cdiv1beta1.DataImportCron{}
@@ -1116,7 +1116,7 @@ var _ = Describe("DataSources", func() {
 					}, timeout, time.Second).Should(Equal(metav1.StatusReasonNotFound), "DataImportCron should not exist.")
 				})
 
-				It("[test_id:8298] should restore DataSource, when CDI label is removed", func() {
+				PIt("[QUARANTINE][test_id:8298] should restore DataSource, when CDI label is removed", func() {
 					// Wait until DataImportCron imports PVC and changes data source
 					Eventually(func() (bool, error) {
 						cron := &cdiv1beta1.DataImportCron{}
