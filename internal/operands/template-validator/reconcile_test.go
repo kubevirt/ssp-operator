9033f312884c03dd033074a6dad118a090236198
Lee Yarwood <lyarwood@redhat.com>
2023-02-15 16:36:19 +0000

    lint: Add ineffassign
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- internal/operands/template-validator/reconcile_test.go
+++ internal/operands/template-validator/reconcile_test.go
@@ -149,7 +149,7 @@ var _ = Describe("Template validator operand", func() {
 	})
 
 	It("should report status", func() {
-		reconcileResults, err := operand.Reconcile(&request)
+		_, err := operand.Reconcile(&request)
 		Expect(err).ToNot(HaveOccurred())
 
 		// Set status for deployment
@@ -162,7 +162,7 @@ var _ = Describe("Template validator operand", func() {
 			deployment.Status.UnavailableReplicas = replicas
 		})
 
-		reconcileResults, err = operand.Reconcile(&request)
+		reconcileResults, err := operand.Reconcile(&request)
 		Expect(err).ToNot(HaveOccurred())
 
 		// Only deployment should be progressing
