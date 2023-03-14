875a70944b2a7094868994bfc2b0fe6ad4e3d423
assaf-admi <aadmi@redhat.com>
2023-03-14 20:32:13 +0200

    Use RUNBOOK_URL_TEMPLATE env for the runbooks URL template
    Signed-off-by: assafad <aadmi@redhat.com>

--- internal/operands/metrics/reconcile_test.go
+++ internal/operands/metrics/reconcile_test.go
@@ -2,6 +2,9 @@ package metrics
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -30,7 +33,7 @@ var _ = Describe("Metrics operand", func() {
 		operand = New()
 	)
 
-	It("should create metrics resources", func() {
+	BeforeEach(func() {
 		client := fake.NewClientBuilder().WithScheme(common.Scheme).Build()
 		request = common.Request{
 			Request: reconcile.Request{
@@ -54,14 +57,55 @@ var _ = Describe("Metrics operand", func() {
 			Logger:       log,
 			VersionCache: common.VersionCache{},
 		}
+	})
 
+	AfterEach(func() {
+		os.Unsetenv(runbookURLTemplateEnv)
+	})
+
+	It("should create metrics resources", func() {
 		_, err := operand.Reconcile(&request)
 		Expect(err).ToNot(HaveOccurred())
-		ExpectResourceExists(newPrometheusRule(namespace), request)
+
+		prometheusRule, err := newPrometheusRule(namespace)
+		Expect(err).ToNot(HaveOccurred())
+
+		ExpectResourceExists(prometheusRule, request)
 		ExpectResourceExists(newServiceMonitorCR(namespace), request)
 		ExpectResourceExists(newMonitoringClusterRole(), request)
 		ExpectResourceExists(newMonitoringClusterRoleBinding(), request)
 	})
+
+	DescribeTable("runbook URL template",
+		func(template string) {
+			if template != defaultRunbookURLTemplate {
+				os.Setenv(runbookURLTemplateEnv, template)
+			}
+
+			prometheusRule, err := newPrometheusRule(namespace)
+
+			if strings.Count(template, "%s") != 1 || strings.Count(template, "%") != 1 {
+				Expect(err).To(HaveOccurred())
+				return
+			}
+
+			Expect(err).ToNot(HaveOccurred())
+
+			for _, group := range prometheusRule.Spec.Groups {
+				for _, rule := range group.Rules {
+					if rule.Alert != "" {
+						if rule.Annotations["runbook_url"] != "" {
+							Expect(rule.Annotations["runbook_url"]).To(Equal(fmt.Sprintf(template, rule.Alert)))
+						}
+					}
+				}
+			}
+		},
+		Entry("should use the default template when no ENV variable is set", defaultRunbookURLTemplate),
+		Entry("should use the ENV variable when a valid value is set", "valid/runbookURL/template/%s"),
+		Entry("should throw an error when the ENV variable value doesn't contain a string format placeholder", "invalid/runbookURL/template/"),
+		Entry("should throw an error when the ENV variable value contains an integer format placeholder", "invalid/runbookURL/template/%d"),
+	)
 })
 
 func TestMetrics(t *testing.T) {
