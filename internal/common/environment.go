9667c4264c1e1f488ee4ff28aa6ed63d5367be53
Karel Šimon <ksimon@redhat.com>
2023-05-16 11:58:26 +0200

    feat: adjust container links
    This change replaces all tekton tasks containers with 2 new.
    CSV generator was updated to accept only two links for tekton containers
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- internal/common/environment.go
+++ internal/common/environment.go
@@ -16,76 +16,27 @@ import (
 )
 
 const (
-	OperatorVersionKey        = "OPERATOR_VERSION"
-	TemplateValidatorImageKey = "VALIDATOR_IMAGE"
-	CleanupVMImageKey         = "CLEANUP_VM_IMG"
-	CopyTemplateImageKey      = "COPY_TEMPLATE_IMG"
-	ModifyDataObjectImageKey  = "MODIFY_DATA_OBJECT_IMG"
-	CreateVMImageKey          = "CREATE_VM_IMG"
-	DiskVirtCustomizeImageKey = "DISK_VIRT_CUSTOMIZE_IMG"
-	DiskVirtSysprepImageKey   = "DISK_VIRT_SYSPREP_IMG"
-	ModifyVMTemplateImageKey  = "MODIFY_VM_TEMPLATE_IMG"
-	WaitForVMISTatusImageKey  = "WAIT_FOR_VMI_STATUS_IMG"
-	VirtioImageKey            = "VIRTIO_IMG"
-	GenerateSSHKeysImageKey   = "GENERATE_SSH_KEYS_IMG"
+	OperatorVersionKey          = "OPERATOR_VERSION"
+	TemplateValidatorImageKey   = "VALIDATOR_IMAGE"
+	TektonTasksImageKey         = "TEKTON_TASKS_IMG"
+	TektonTasksDiskVirtImageKey = "TEKTON_TASKS_DISK_VIRT_IMG"
+	VirtioImageKey              = "VIRTIO_IMG"
 
-	DefaultWaitForVMIStatusIMG  = "quay.io/kubevirt/tekton-task-wait-for-vmi-status:" + TektonTasksVersion
-	DeafultModifyVMTemplateIMG  = "quay.io/kubevirt/tekton-task-modify-vm-template:" + TektonTasksVersion
-	DeafultDiskVirtSysprepIMG   = "quay.io/kubevirt/tekton-task-disk-virt-sysprep:" + TektonTasksVersion
-	DeafultDiskVirtCustomizeIMG = "quay.io/kubevirt/tekton-task-disk-virt-customize:" + TektonTasksVersion
-	DeafultCreateVMIMG          = "quay.io/kubevirt/tekton-task-create-vm:" + TektonTasksVersion
-	DeafultModifyDataObjectIMG  = "quay.io/kubevirt/tekton-task-modify-data-object:" + TektonTasksVersion
-	DeafultCopyTemplateIMG      = "quay.io/kubevirt/tekton-task-copy-template:" + TektonTasksVersion
-	DeafultCleanupVMIMG         = "quay.io/kubevirt/tekton-task-execute-in-vm:" + TektonTasksVersion
-	GenerateSSHKeysIMG          = "quay.io/kubevirt/tekton-task-generate-ssh-keys:" + TektonTasksVersion
-	DefaultVirtioIMG            = "quay.io/kubevirt/virtio-container-disk:v0.59.0"
+	DefaultTektonTasksIMG         = "quay.io/kubevirt/tekton-tasks:" + TektonTasksVersion
+	DeafultTektonTasksDiskVirtIMG = "quay.io/kubevirt/tekton-tasks-disk-virt:" + TektonTasksVersion
+	DefaultVirtioIMG              = "quay.io/kubevirt/virtio-container-disk:v0.59.0"
 
 	defaultOperatorVersion = "devel"
 )
 
 // GetSSHKeysStatusImage returns generate-ssh-keys task image url
-func GetSSHKeysStatusImage() string {
-	return EnvOrDefault(GenerateSSHKeysImageKey, GenerateSSHKeysIMG)
-}
-
-// GetWaitForVMIStatusImage returns wait-for-vmi-status task image url
-func GetWaitForVMIStatusImage() string {
-	return EnvOrDefault(WaitForVMISTatusImageKey, DefaultWaitForVMIStatusIMG)
-}
-
-// GetModifyVMTemplateImage returns modify-vm-template task image url
-func GetModifyVMTemplateImage() string {
-	return EnvOrDefault(ModifyVMTemplateImageKey, DeafultModifyVMTemplateIMG)
+func GetTektonTasksImage() string {
+	return EnvOrDefault(TektonTasksImageKey, DefaultTektonTasksIMG)
 }
 
 // GetDiskVirtSysprepImage returns disk-virt-sysprep task image url
-func GetDiskVirtSysprepImage() string {
-	return EnvOrDefault(DiskVirtSysprepImageKey, DeafultDiskVirtSysprepIMG)
-}
-
-// GetDiskVirtCustomizeImage returns disk-virt-customize task image url
-func GetDiskVirtCustomizeImage() string {
-	return EnvOrDefault(DiskVirtCustomizeImageKey, DeafultDiskVirtCustomizeIMG)
-}
-
-// GetCreateVMImage returns create-vm-from-manifest task image url
-func GetCreateVMImage() string {
-	return EnvOrDefault(CreateVMImageKey, DeafultCreateVMIMG)
-}
-
-// GetModifyDataObjectImage returns modify-data-object task image url
-func GetModifyDataObjectImage() string {
-	return EnvOrDefault(ModifyDataObjectImageKey, DeafultModifyDataObjectIMG)
-}
-
-// GetCopyTemplatemage returns copy-template task image url
-func GetCopyTemplateImage() string {
-	return EnvOrDefault(CopyTemplateImageKey, DeafultCopyTemplateIMG)
-}
-
-// GetCleanupVMImage returns cleanup-vm task image url
-func GetCleanupVMImage() string {
-	return EnvOrDefault(CleanupVMImageKey, DeafultCleanupVMIMG)
+func GetTektonTasksDiskVirtImage() string {
+	return EnvOrDefault(TektonTasksDiskVirtImageKey, DeafultTektonTasksDiskVirtIMG)
 }
 
 // GetVirtioImage returns virtio image url
