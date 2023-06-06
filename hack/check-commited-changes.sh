e019de1211549065ee85c4728a0e3bb7c2192793
Ondrej Pokorny <opokorny@redhat.com>
2023-06-06 16:09:12 +0200

    feat: Add checking that all files are commited
    This commit modifies the Makefile to execute script
    that checks whether all generated files are committed.
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

---
+++ hack/check-commited-changes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+check_make() {
+    if ! make "$1"; then
+        echo "make $1 failed"
+        exit 1
+    fi
+}
+
+check_make vendor
+check_make generate
+check_make manifests
+check_make fmt
+
+# check git status
+status=$(git status --porcelain)
+if [[ -n $status ]]; then
+    echo "There are uncommitted changes."
+    exit 1
+fi
