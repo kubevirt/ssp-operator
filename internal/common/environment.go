73a90e9b1c22bef69aab86a8985bc88e87e0c466
Kubevirt tekton tasks operator Update Automation <ksimon@redhat.com>
2023-11-27 08:08:22 +0000

    Update virtio image version to v1.1.0
    Signed-off-by: Kubevirt tekton tasks operator Update Automation <ksimon@redhat.com>

--- internal/common/environment.go
+++ internal/common/environment.go
@@ -25,7 +25,7 @@ const (
 
 	DefaultTektonTasksIMG         = "quay.io/kubevirt/tekton-tasks:" + TektonTasksVersion
 	DeafultTektonTasksDiskVirtIMG = "quay.io/kubevirt/tekton-tasks-disk-virt:" + TektonTasksVersion
-	DefaultVirtioIMG              = "quay.io/kubevirt/virtio-container-disk:v1.0.1"
+	DefaultVirtioIMG              = "quay.io/kubevirt/virtio-container-disk:v1.1.0"
 
 	defaultOperatorVersion = "devel"
 )
