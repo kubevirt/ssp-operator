055f66f4948929b2320f9d70c5af36b3c2063587
Andrej Krejcir <akrejcir@redhat.com>
2023-11-30 15:53:17 +0100

    deps: Update controller-runtime to v0.16.3
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- internal/operands/vm-console-proxy/reconcile_test.go
+++ internal/operands/vm-console-proxy/reconcile_test.go
@@ -198,12 +198,12 @@ var _ = Describe("VM Console Proxy Operand", func() {
 
 		// Set status for deployment
 		key := client.ObjectKeyFromObject(bundle.Deployment)
-		updateDeployment(key, &request, func(deployment *apps.Deployment) {
-			deployment.Status.Replicas = replicas
-			deployment.Status.ReadyReplicas = 0
-			deployment.Status.AvailableReplicas = 0
-			deployment.Status.UpdatedReplicas = 0
-			deployment.Status.UnavailableReplicas = replicas
+		updateDeploymentStatus(key, &request, func(deploymentStatus *apps.DeploymentStatus) {
+			deploymentStatus.Replicas = replicas
+			deploymentStatus.ReadyReplicas = 0
+			deploymentStatus.AvailableReplicas = 0
+			deploymentStatus.UpdatedReplicas = 0
+			deploymentStatus.UnavailableReplicas = replicas
 		})
 
 		reconcileResults, err := operand.Reconcile(&request)
@@ -222,12 +222,12 @@ var _ = Describe("VM Console Proxy Operand", func() {
 			}
 		}
 
-		updateDeployment(key, &request, func(deployment *apps.Deployment) {
-			deployment.Status.Replicas = replicas
-			deployment.Status.ReadyReplicas = replicas
-			deployment.Status.AvailableReplicas = replicas
-			deployment.Status.UpdatedReplicas = replicas
-			deployment.Status.UnavailableReplicas = 0
+		updateDeploymentStatus(key, &request, func(deploymentStatus *apps.DeploymentStatus) {
+			deploymentStatus.Replicas = replicas
+			deploymentStatus.ReadyReplicas = replicas
+			deploymentStatus.AvailableReplicas = replicas
+			deploymentStatus.UpdatedReplicas = replicas
+			deploymentStatus.UnavailableReplicas = 0
 		})
 
 		reconcileResults, err = operand.Reconcile(&request)
@@ -530,10 +530,9 @@ func getMockedTestBundle() *vm_console_proxy_bundle.Bundle {
 	}
 }
 
-func updateDeployment(key client.ObjectKey, request *common.Request, updateFunc func(deployment *apps.Deployment)) {
+func updateDeploymentStatus(key client.ObjectKey, request *common.Request, updateFunc func(deploymentStatus *apps.DeploymentStatus)) {
 	deployment := &apps.Deployment{}
 	Expect(request.Client.Get(request.Context, key, deployment)).ToNot(HaveOccurred())
-	updateFunc(deployment)
-	Expect(request.Client.Update(request.Context, deployment)).ToNot(HaveOccurred())
+	updateFunc(&deployment.Status)
 	Expect(request.Client.Status().Update(request.Context, deployment)).ToNot(HaveOccurred())
 }
