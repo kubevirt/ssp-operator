38bf5eee13c63b552b8c5ec1672b930163550382
Andrej Krejcir <akrejcir@redhat.com>
2024-03-04 13:44:03 +0000

    chore: update vm-console-proxy manifests to v0.5.0
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- internal/operands/vm-console-proxy/defaults.go
+++ internal/operands/vm-console-proxy/defaults.go
@@ -3,6 +3,6 @@ package vm_console_proxy
 // This file is updated by GitHub action defined in .github/workflows/release-vm-console-proxy.yaml
 
 const (
-	defaultVmConsoleProxyImageTag = "v0.4.0"
+	defaultVmConsoleProxyImageTag = "v0.5.0"
 	defaultVmConsoleProxyImage    = "quay.io/kubevirt/vm-console-proxy:" + defaultVmConsoleProxyImageTag
 )
