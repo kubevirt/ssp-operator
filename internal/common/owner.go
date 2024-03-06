f7940f630c9047606a9db20e6e716a3bf472a3b9
Andrej Krejcir <akrejcir@redhat.com>
2024-03-06 17:25:51 +0100

    code change: Move function to the internal/common package
    The function ListOwnedResources() will be used by following commits.
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- internal/common/owner.go
+++ internal/common/owner.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"reflect"
+
 	"github.com/operator-framework/operator-lib/handler"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -25,4 +27,37 @@ func CheckOwnerAnnotation(obj client.Object, owner client.Object) bool {
 
 	namespacedNameVal, ok := annotations[handler.NamespacedNameAnnotation]
 	return ok && namespacedNameVal == sspNamespacedName.String()
+}
+
+func ListOwnedResources[L any, T any, PtrL interface {
+	*L
+	client.ObjectList
+}, PtrT interface {
+	*T
+	client.Object
+}](request *Request, listOpts ...client.ListOption) ([]T, error) {
+	listObj := PtrL(new(L))
+	if err := request.Client.List(request.Context, listObj, listOpts...); err != nil {
+		return nil, err
+	}
+
+	items := extractItems[PtrL, T](listObj)
+	var filteredItems []T
+	for _, item := range items {
+		if !CheckOwnerAnnotation(PtrT(&item), request.Instance) {
+			continue
+		}
+		filteredItems = append(filteredItems, item)
+	}
+	return filteredItems, nil
+}
+
+func extractItems[L client.ObjectList, T any](list L) []T {
+	listValue := reflect.ValueOf(list)
+	if !listValue.IsValid() {
+		return nil
+	}
+
+	itemsValue := listValue.Elem().FieldByName("Items")
+	return itemsValue.Interface().([]T)
 }
