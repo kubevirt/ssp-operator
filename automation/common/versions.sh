dd467f91d5da52019157945959f74f1a6843acaf
Andrej Krejcir <akrejcir@redhat.com>
2024-07-31 16:26:18 +0200

    CI: Fix versions of Kubevirt and CDI to ones used in 4.16
    The CI should install compatible Kubevirt and CDI versions.
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- automation/common/versions.sh
+++ automation/common/versions.sh
@@ -53,11 +53,11 @@ function latest_version() {
     tail -n1
 }
 
-# Latest released Kubevirt version
-KUBEVIRT_VERSION=$(latest_version "kubevirt" "kubevirt")
+# Fix kubevirt version to v1.2.x
+KUBEVIRT_VERSION=$(latest_patch_version "kubevirt" "v1.2")
 
-# Latest released CDI version
-CDI_VERSION=$(latest_version "kubevirt" "containerized-data-importer")
+# Fix CDI version to v.1.59.x
+CDI_VERSION=$(latest_patch_version "containerized-data-importer" "v1.59")
 
 # Latest released Tekton version
 TEKTON_VERSION=$(latest_version "tektoncd" "operator")
