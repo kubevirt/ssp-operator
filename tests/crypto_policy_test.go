240a1883cfeb8a3bcb3dbd41022c3aa3ed898bea
Andrej Krejcir <akrejcir@redhat.com>
2024-10-30 16:49:05 +0100

    tests: Modify TLS policy tests to check validator
    Check TLS policy of template validator pod.
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- tests/crypto_policy_test.go
+++ tests/crypto_policy_test.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	template_validator "kubevirt.io/ssp-operator/internal/operands/template-validator"
 
 	ocpv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/library-go/pkg/crypto"
@@ -109,9 +111,17 @@ var _ = Describe("Crypto Policy", func() {
 	Context("setting Crypto Policy", func() {
 		DescribeTable("Adhere to defined TLSConfig", func(tlsConfigTestPermutation tlsConfigTestPermutation) {
 			pod := operatorPod()
+			validatorPod := templateValidatorPod()
+
 			applyTLSConfig(tlsConfigTestPermutation.openshiftTLSPolicy)
 			Expect(testMetricsEndpoint(pod, tlsConfigTestPermutation)).To(Succeed())
 			Expect(testWebhookEndpoint(pod, tlsConfigTestPermutation)).To(Succeed())
+
+			// Using larger timeout, because it usually takes more than a minute
+			// for the ConfigMap to be propagated to the pod.
+			Eventually(func() error {
+				return testValidatorEndpoint(validatorPod, tlsConfigTestPermutation)
+			}, env.Timeout(), time.Second).Should(Succeed())
 		},
 			Entry("[test_id:9360] old", oldPermutation),
 			Entry("[test_id:9276] intermediate", intermediatePermutation),
@@ -127,6 +137,17 @@ func operatorPod() core.Pod {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(pods.Items).ToNot(BeEmpty())
 	Expect(pods.Items).To(HaveLen(1))
+	return pods.Items[0]
+}
+
+func templateValidatorPod() core.Pod {
+	pods := &core.PodList{}
+	err := apiClient.List(context.TODO(), pods, client.MatchingLabels{
+		common.AppKubernetesNameLabel:      "template-validator",
+		common.AppKubernetesComponentLabel: string(common.AppComponentTemplating),
+	})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(pods.Items).ToNot(BeEmpty())
 	return pods.Items[0]
 }
 
@@ -248,6 +269,10 @@ func testMetricsEndpoint(pod core.Pod, tlsConfig tlsConfigTestPermutation) error
 
 func testWebhookEndpoint(pod core.Pod, tlsConfig tlsConfigTestPermutation) error {
 	return tlsConfig.testEndpointAccessabilityWithTLS(pod, strategy.GetSSPWebhookServiceName(), "", 9443)
+}
+
+func testValidatorEndpoint(pod core.Pod, tlsConfig tlsConfigTestPermutation) error {
+	return tlsConfig.testEndpointAccessabilityWithTLS(pod, template_validator.ServiceName, "", 8443)
 }
 
 func applyTLSConfig(tlsSecurityProfile *ocpv1.TLSSecurityProfile) {
