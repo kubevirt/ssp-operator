6f391094d7bf1e36909d708e6bbbf226cb692207
Andrej Krejcir <akrejcir@redhat.com>
2025-06-09 16:25:06 +0200

    chore: refactor template-bundle code
    - Removed the Bundle struct, and made two functions public.
    - The template-bundle returns DataSource names instead
    of a slice of DataSources.
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- internal/template-bundle/bundle_test.go
+++ internal/template-bundle/bundle_test.go
@@ -6,6 +6,13 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	templatev1 "github.com/openshift/api/template/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/json"
+	kubevirtv1 "kubevirt.io/api/core/v1"
+	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
 )
 
 func retrieveSuffix() string {
@@ -17,65 +24,105 @@ func retrieveSuffix() string {
 	}
 }
 
-var _ = Describe("Template bundle", Ordered, func() {
-	var (
-		testBundle Bundle
-		nameSuffix string
-	)
-
-	BeforeAll(func() {
-		var err error
-		testBundle, err = ReadBundle("template-bundle-test.yaml")
+var _ = Describe("Template bundle", func() {
+	It("ReadTemplates() should correctly read templates", func() {
+		testTemplates, err := ReadTemplates("template-bundle-test.yaml")
 		Expect(err).ToNot(HaveOccurred())
-		nameSuffix = retrieveSuffix()
-	})
 
-	It("should correctly read templates", func() {
-		templates := testBundle.Templates
-		Expect(templates).To(HaveLen(4))
+		nameSuffix := retrieveSuffix()
+
+		Expect(testTemplates).To(HaveLen(4))
 		{
-			templ := templates[0]
+			templ := testTemplates[0]
 			Expect(templ.Name).To(Equal("centos-stream8-server-medium" + nameSuffix))
 			Expect(templ.Annotations).To(HaveKey("name.os.template.kubevirt.io/centos-stream8"))
 			Expect(templ.Objects).To(HaveLen(1))
 		}
 		{
-			templ := templates[1]
+			templ := testTemplates[1]
 			Expect(templ.Name).To(Equal("centos-stream8-desktop-large" + nameSuffix))
 			Expect(templ.Annotations).To(HaveKey("name.os.template.kubevirt.io/centos-stream8"))
 			Expect(templ.Objects).To(HaveLen(1))
 		}
 		{
-			templ := templates[2]
+			templ := testTemplates[2]
 			Expect(templ.Name).To(Equal("windows10-desktop-medium" + nameSuffix))
 			Expect(templ.Annotations).To(HaveKey("name.os.template.kubevirt.io/win10"))
 			Expect(templ.Objects).To(HaveLen(1))
 		}
 		{
-			templ := templates[3]
+			templ := testTemplates[3]
 			Expect(templ.Name).To(Equal("rhel8-saphana-tiny" + nameSuffix))
 			Expect(templ.Annotations).To(HaveKey("name.os.template.kubevirt.io/rhel8.4"))
 			Expect(templ.Objects).To(HaveLen(1))
 		}
 	})
 
-	It("should create DataSources", func() {
-		dataSources := testBundle.DataSources
-		Expect(dataSources).To(HaveLen(2))
+	Context("CollectDataSourceNames", func() {
+		It("should collect DataSource names", func() {
+			// The template object is not strictly a VirtualMachine, because it can contain
+			// string variables in fields that don't have string type. But for this test code,
+			// we don't use any such variables.
+			testVmTemplate := &kubevirtv1.VirtualMachine{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "VirtualMachine",
+					APIVersion: kubevirtv1.GroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec: kubevirtv1.VirtualMachineSpec{
+					DataVolumeTemplates: []kubevirtv1.DataVolumeTemplateSpec{{
+						Spec: cdiv1beta1.DataVolumeSpec{
+							SourceRef: &cdiv1beta1.DataVolumeSourceRef{},
+						},
+					}},
+				},
+			}
 
-		ds1 := dataSources[0]
-		Expect(ds1.Name).To(Equal("centos-stream8"))
-		Expect(ds1.Namespace).To(Equal("kubevirt-os-images"))
-		Expect(ds1.Spec.Source.PVC.Name).To(Equal("centos-stream8"))
-		Expect(ds1.Spec.Source.PVC.Namespace).To(Equal("kubevirt-os-images"))
+			testTemplates := []templatev1.Template{{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "centos-stream8-server-medium",
+				},
+				Objects: []k8sruntime.RawExtension{{
+					Object: testVmTemplate,
+				}},
+				Parameters: []templatev1.Parameter{{
+					Name:  "DATA_SOURCE_NAME",
+					Value: "centos-stream8",
+				}, {
+					Name:  "DATA_SOURCE_NAMESPACE",
+					Value: "kubevirt-os-images",
+				}},
+			}, {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "windows10-desktop-medium",
+				},
+				Objects: []k8sruntime.RawExtension{{
+					Object: testVmTemplate,
+				}},
+				Parameters: []templatev1.Parameter{{
+					Name:  "DATA_SOURCE_NAME",
+					Value: "win10",
+				}, {
+					Name:  "DATA_SOURCE_NAMESPACE",
+					Value: "kubevirt-os-images",
+				}},
+			}}
 
-		ds2 := dataSources[1]
-		Expect(ds2.Name).To(Equal("win10"))
-		Expect(ds2.Namespace).To(Equal("kubevirt-os-images"))
-		Expect(ds2.Spec.Source.PVC.Name).To(Equal("win10"))
-		Expect(ds2.Spec.Source.PVC.Namespace).To(Equal("kubevirt-os-images"))
+			for i := range testTemplates {
+				for j := range testTemplates[i].Objects {
+					object := &testTemplates[i].Objects[j]
+					var err error
+					object.Raw, err = json.Marshal(object.Object)
+					Expect(err).ToNot(HaveOccurred())
+				}
+			}
+
+			dataSourceNames, err := CollectDataSourceNames(testTemplates)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(dataSourceNames).To(ConsistOf("centos-stream8", "win10"))
+		})
 	})
-
 })
 
 func TestTemplateBundle(t *testing.T) {
