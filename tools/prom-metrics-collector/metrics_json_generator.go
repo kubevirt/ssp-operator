cee2b5706fb653f340231183bf1d133acad79f7b
Aviv Litman <alitman@alitman-thinkpadp1gen7.raanaii.csb>
2025-12-11 15:17:02 +0200

    Fix metric names linter to handle recording rules separately
    This pr lint-metrics to send recording rules and metrics as 2 list (in
    the same file as before) to metrics name linter which handle them
    separately.
    
    Signed-off-by: alitman <alitman@alitman-thinkpadp1gen7.raanaii.csb>
    Signed-off-by: Aviv Litman <alitman@alitman-thinkpadp1gen7.raanaii.csb>

--- tools/prom-metrics-collector/metrics_json_generator.go
+++ tools/prom-metrics-collector/metrics_json_generator.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/kubevirt/monitoring/pkg/metrics/parser"
+	dto "github.com/prometheus/client_model/go"
 
 	sspMetrics "kubevirt.io/ssp-operator/pkg/monitoring/metrics/ssp-operator"
 	validatorMetrics "kubevirt.io/ssp-operator/pkg/monitoring/metrics/template-validator"
@@ -15,8 +16,17 @@ import (
 // This should be used only for very rare cases where the naming conventions that are explained in the best practices:
 // https://sdk.operatorframework.io/docs/best-practices/observability-best-practices/#metrics-guidelines
 // should be ignored.
-var excludedMetrics = map[string]struct{}{
-	"cnv:vmi_status_running:count": {},
+var excludedMetrics = map[string]struct{}{}
+
+type RecordingRule struct {
+	Record string `json:"record,omitempty"`
+	Expr   string `json:"expr,omitempty"`
+	Type   string `json:"type,omitempty"`
+}
+
+type Output struct {
+	MetricFamilies []*dto.MetricFamily `json:"metricFamilies,omitempty"`
+	RecordingRules []RecordingRule     `json:"recordingRules,omitempty"`
 }
 
 func main() {
@@ -32,31 +42,50 @@ func main() {
 		panic(err)
 	}
 
-	var metricFamilies []parser.Metric
+	var metricFamilies []*dto.MetricFamily
 
 	metricsList := sspMetrics.ListMetrics()
 	for _, m := range metricsList {
 		if _, isExcludedMetric := excludedMetrics[m.GetOpts().Name]; !isExcludedMetric {
-			metricFamilies = append(metricFamilies, parser.Metric{
+			pm := parser.Metric{
 				Name: m.GetOpts().Name,
 				Help: m.GetOpts().Help,
 				Type: strings.ToUpper(string(m.GetBaseType())),
-			})
+			}
+			metricFamilies = append(metricFamilies, parser.CreateMetricFamily(pm))
 		}
 	}
 
+	recNames := make(map[string]struct{})
+	var recRules []RecordingRule
 	rulesList := rules.ListRecordingRules()
 	for _, r := range rulesList {
-		if _, isExcludedMetric := excludedMetrics[r.GetOpts().Name]; !isExcludedMetric {
-			metricFamilies = append(metricFamilies, parser.Metric{
-				Name: r.GetOpts().Name,
-				Help: r.GetOpts().Help,
-				Type: strings.ToUpper(string(r.GetType())),
-			})
+		name := r.GetOpts().Name
+		if _, isExcludedMetric := excludedMetrics[name]; isExcludedMetric {
+			continue
 		}
+		recNames[name] = struct{}{}
+		recRules = append(recRules, RecordingRule{
+			Record: name,
+			Expr:   r.Expr.String(),
+			Type:   strings.ToUpper(string(r.GetType())),
+		})
 	}
 
-	if jsonBytes, err := json.Marshal(metricFamilies); err != nil {
+	// Filter out metric families that are also recording rules
+	var filteredFamilies []*dto.MetricFamily
+	for _, mf := range metricFamilies {
+		if mf == nil || mf.Name == nil {
+			continue
+		}
+		if _, isRec := recNames[*mf.Name]; isRec {
+			continue
+		}
+		filteredFamilies = append(filteredFamilies, mf)
+	}
+
+	out := Output{MetricFamilies: filteredFamilies, RecordingRules: recRules}
+	if jsonBytes, err := json.Marshal(out); err != nil {
 		panic(err)
 	} else {
 		fmt.Println(string(jsonBytes))
